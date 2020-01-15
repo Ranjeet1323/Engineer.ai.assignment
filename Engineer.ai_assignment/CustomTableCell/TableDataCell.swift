71fa5501bff18a458782172b36fc8b99e035764a
Ranjeet <rschauhan@svam.com>
2020-01-15 12:50:51 +0530

    done

---
+++ Engineer.ai_assignment/CustomTableCell/TableDataCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableDataCell.swift
+//  Engineer.ai_assignment
+//
+//  Created by Ranjeet on 15/01/20.
+//  Copyright © 2020 Ranjeet. All rights reserved.
+//
+
+import UIKit
+
+class TableDataCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Engineer.ai_assignment/Model/tableDataDS.swift
@@ -0,0 +1,13 @@
+//
+//  tableDataDS.swift
+//  Engineer.ai_assignment
+//
+//  Created by Ranjeet on 15/01/20.
+//  Copyright © 2020 Ranjeet. All rights reserved.
+//
+
+import UIKit
+
+class tableDataDS: NSObject {
+
+}
